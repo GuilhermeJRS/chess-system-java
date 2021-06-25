72c5a273e35365e52448c91d9e1d4d376c53b9fd
Guilherme Souza <guilhermejrs@gmail.com>
2021-06-25 16:23:24 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.InputMismatchException;
-import java.util.MissingFormatArgumentException;
 import java.util.Scanner;
 
 import chess.ChessPiece;
@@ -31,6 +30,11 @@ public class UI {
 	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
 	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+	public static void clearScreen() {
+		System.out.print("\033[H\033[2J");
+		System.out.flush();
+	}
+
 	public static ChessPosition readChessposition(Scanner sc) {
 		try {
 			String s = sc.nextLine();
@@ -44,7 +48,6 @@ public class UI {
 	}
 
 	public static void printBoard(ChessPiece[][] pieces) {
-
 		for (int i = 0; i < pieces.length; i++) {
 			System.out.print((8 - i) + " ");
 			for (int j = 0; j < pieces[0].length; j++) {
